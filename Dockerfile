6517364fa8d11c922954412c32d32c8b28d2d940
Matvii Butenov <matvej1888@gmail.com>
2025-02-16 12:53:35 +0100

    chore: init project

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM eclipse-temurin
+
+RUN apt-get update
+RUN apt-get install -y maven
+
+COPY . /job-platform
+
+WORKDIR /job-platform
+
+RUN ./mvnw dependency:go-offline
+
+ENV WAIT_VERSION 2.7.2
+ADD https://github.com/ufoscout/docker-compose-wait/releases/download/$WAIT_VERSION/wait /wait
+RUN chmod +x /wait
+
+CMD ["/wait", "mvn", "spring-boot:run"]
---
+++ src/main/java/com/butenov/jobplatform/users/User.java
@@ -0,0 +1,26 @@
+package com.butenov.jobplatform.users;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+}
