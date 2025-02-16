5167793f84fff48f5b1d46220a30c1a375caa940
Matvii Butenov <matvej1888@gmail.com>
2025-02-16 10:10:57 +0100

    chore: init project

---
+++ src/test/java/com/butenov/jobplatform/JobPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.butenov.jobplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
